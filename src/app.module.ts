bed003178276c556240e2d2474ba30643c90aa5c
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2020-09-21 16:48:13 +0900

    1.0 Apollo Server Setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [GraphQLModule.forRoot()],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
