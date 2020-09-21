e1b99dc051b8eed2fc0bbb327b390d052ab9970b
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2020-09-21 17:39:12 +0900

    1.3 Arguments

--- src/restaurants/restaurants.resolver.ts
+++ src/restaurants/restaurants.resolver.ts
@@ -1,10 +1,10 @@
-import { Query, Resolver } from '@nestjs/graphql';
+import { Args, Query, Resolver } from '@nestjs/graphql';
 import { Restaurant } from './entities/restaurant.entity';
 
-@Resolver()
+@Resolver(of => Restaurant)
 export class RestaurantResolver {
-  @Query(returns => Restaurant)
-  myRestaurant() {
-    return true;
+  @Query(returns => [Restaurant])
+  restaurants(@Args('veganOnly') veganOnly: boolean): Restaurant[] {
+    return [];
   }
 }
