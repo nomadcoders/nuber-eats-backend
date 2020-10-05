f838334b94dad33ae2472073be176f6d1ddf37e2
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2020-10-05 21:41:04 +0900

    4.6.An Alternative Error

--- src/users/users.resolver.ts
+++ src/users/users.resolver.ts
@@ -20,15 +20,12 @@ export class UserResolver {
     @Args('input') createAccountInput: CreateAccountInput,
   ): Promise<CreateAccountOutput> {
     try {
-      const error = await this.usersService.createAccount(createAccountInput);
-      if (error) {
-        return {
-          ok: false,
-          error,
-        };
-      }
+      const { ok, error } = await this.usersService.createAccount(
+        createAccountInput,
+      );
       return {
-        ok: true,
+        ok,
+        error,
       };
     } catch (error) {
       return {
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -14,15 +14,16 @@ export class UserService {
     email,
     password,
     role,
-  }: CreateAccountInput): Promise<string | undefined> {
+  }: CreateAccountInput): Promise<{ ok: boolean; error?: string }> {
     try {
       const exists = await this.users.findOne({ email });
       if (exists) {
-        return 'There is a user with that email already';
+        return { ok: false, error: 'There is a user with that email already' };
       }
       await this.users.save(this.users.create({ email, password, role }));
+      return { ok: true };
     } catch (e) {
-      return "Couldn't create account";
+      return { ok: false, error: "Couldn't create account" };
     }
   }
 }
