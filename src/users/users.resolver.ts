a092b994c5e7197d58350e8afdf0ec8eb56e9d4e
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2020-10-05 20:33:18 +0900

    4.5.Create Account Mutation part Three

--- src/users/users.resolver.ts
+++ src/users/users.resolver.ts
@@ -16,5 +16,25 @@ export class UserResolver {
   }
 
   @Mutation(returns => CreateAccountOutput)
-  createAccount(@Args('input') createAccountInput: CreateAccountInput) {}
+  async createAccount(
+    @Args('input') createAccountInput: CreateAccountInput,
+  ): Promise<CreateAccountOutput> {
+    try {
+      const error = await this.usersService.createAccount(createAccountInput);
+      if (error) {
+        return {
+          ok: false,
+          error,
+        };
+      }
+      return {
+        ok: true,
+      };
+    } catch (error) {
+      return {
+        error,
+        ok: false,
+      };
+    }
+  }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -10,18 +10,19 @@ export class UserService {
     @InjectRepository(User) private readonly users: Repository<User>,
   ) {}
 
-  async createAccount({ email, password, role }: CreateAccountInput) {
+  async createAccount({
+    email,
+    password,
+    role,
+  }: CreateAccountInput): Promise<string | undefined> {
     try {
       const exists = await this.users.findOne({ email });
       if (exists) {
-        // make error
-        return;
+        return 'There is a user with that email already';
       }
       await this.users.save(this.users.create({ email, password, role }));
-      return true;
     } catch (e) {
-      //make error
-      return;
+      return "Couldn't create account";
     }
   }
 }
