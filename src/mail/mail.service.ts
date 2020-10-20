57242fc32397493b984c9ae2eb9b716e102c7767
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2020-10-20 18:06:07 +0900

    6.8 Beautiful Emails

--- src/mail/mail.service.ts
+++ src/mail/mail.service.ts
@@ -17,7 +17,9 @@ export class MailService {
     form.append('from', `Excited User <mailgun@${this.options.domain}>`);
     form.append('to', `nico@nomadcoders.co`);
     form.append('subject', subject);
-    form.append('text', content);
+    form.append('template', 'verify-email');
+    form.append('v:code', 'asasas');
+    form.append('v:username', 'nico!!!');
     const response = await got(
       `https://api.mailgun.net/v3/${this.options.domain}/messages`,
       {
