254f6e88509729ad30e5be11518997509a8269e9
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2020-10-26 16:19:45 +0900

    8.3 sendVerificationEmail Test

--- src/mail/mail.service.spec.ts
+++ src/mail/mail.service.spec.ts
@@ -33,6 +33,27 @@ describe('MailService', () => {
     expect(service).toBeDefined();
   });
 
+  describe('sendVerificationEmail', () => {
+    it('should call sendEmail', () => {
+      const sendVerificationEmailArgs = {
+        email: 'email',
+        code: 'code',
+      };
+      jest.spyOn(service, 'sendEmail').mockImplementation(async () => {});
+      service.sendVerificationEmail(
+        sendVerificationEmailArgs.email,
+        sendVerificationEmailArgs.code,
+      );
+      expect(service.sendEmail).toHaveBeenCalledTimes(1);
+      expect(service.sendEmail).toHaveBeenCalledWith(
+        'Verify Your Email',
+        'verify-email',
+        [
+          { key: 'code', value: sendVerificationEmailArgs.code },
+          { key: 'username', value: sendVerificationEmailArgs.email },
+        ],
+      );
+    });
+  });
   it.todo('sendEmail');
-  it.todo('sendVerificationEmail');
 });
--- src/mail/mail.service.ts
+++ src/mail/mail.service.ts
@@ -10,11 +10,7 @@ export class MailService {
     @Inject(CONFIG_OPTIONS) private readonly options: MailModuleOptions,
   ) {}
 
-  private async sendEmail(
-    subject: string,
-    template: string,
-    emailVars: EmailVar[],
-  ) {
+  async sendEmail(subject: string, template: string, emailVars: EmailVar[]) {
     const form = new FormData();
     form.append(
       'from',
