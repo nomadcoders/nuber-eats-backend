0326cbc2886f59b722e75895e011a2f0e6c971e7
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2020-10-26 15:08:18 +0900

    8.0 JWT Service Test Setup

---
+++ src/jwt/jwt.service.spec.ts
@@ -0,0 +1,26 @@
+import { Test } from '@nestjs/testing';
+import { CONFIG_OPTIONS } from 'src/common/common.constants';
+import { JwtService } from './jwt.service';
+
+const TEST_KEY = 'testKey';
+
+describe('JwtService', () => {
+  let service: JwtService;
+  beforeEach(async () => {
+    const module = await Test.createTestingModule({
+      providers: [
+        JwtService,
+        {
+          provide: CONFIG_OPTIONS,
+          useValue: { privateKey: TEST_KEY },
+        },
+      ],
+    }).compile();
+    service = module.get<JwtService>(JwtService);
+  });
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+  });
+  it.todo('sign');
+  it.todo('verify');
+});
