238bf28cc5a064bcc7a21e71b5e206d785702472
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2020-10-27 19:32:39 +0900

    9.2 Testing createAccount part One

--- test/users.e2e-spec.ts
+++ test/users.e2e-spec.ts
@@ -22,7 +22,35 @@ describe('UserModule (e2e)', () => {
     app.close();
   });
 
-  it.todo('can createAccount');
+  describe('createAccount', () => {
+    const EMAIL = 'nico@las.com';
+    it('should create account', () => {
+      return request(app.getHttpServer())
+        .post(GRAPHQL_ENDPOINT)
+        .send({
+          query: `
+          mutation {
+            createAccount(input: {
+              email:"${EMAIL}",
+              password:"12345",
+              role:Owner
+            }) {
+              ok
+              error
+            }
+          }
+          `,
+        })
+        .expect(200)
+        .expect(res => {
+          expect(res.body.data.createAccount.ok).toBe(true);
+          expect(res.body.data.createAccount.error).toBe(null);
+        });
+    });
+
+    it.todo('should fail if account already exists');
+  });
+
   it.todo('userProfile');
   it.todo('login');
   it.todo('me');
