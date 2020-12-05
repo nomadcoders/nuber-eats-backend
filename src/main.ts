e9bf76db8acd6979ab1a2f141446ffc05b143cb9
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2020-12-05 16:18:25 +0900

    PORT

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors();
-  await app.listen(4000);
+  await app.listen(process.env.PORT || 4000);
 }
 bootstrap();
